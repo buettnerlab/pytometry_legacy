ed7460c1e78d3fcd34b010381cd716e54461f78e
Felix-Hmpl <60603136+Felix-Hmpl@users.noreply.github.com>
2020-09-13 22:58:28 +0200

    Update _hsne.py

--- pytometry/HSNE/pl/_hsne.py
+++ pytometry/HSNE/pl/_hsne.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 
-def hsne(adata, channels_to_plot=None, scale_num=-1, subplot_grid_dim=(1, 1)):
+def hsne(adata, color=None, scale_num=-1, subplot_grid_dim=(1, 1)):
     try:
         adata.uns['hsne_scales']
     except KeyError as e:
@@ -12,14 +12,14 @@ def hsne(adata, channels_to_plot=None, scale_num=-1, subplot_grid_dim=(1, 1)):
     settings = adata.uns['hsne_settings']
     all_channel_names = list(adata.var_names.values)
     all_calculated_channel_names = adata.var_names.values[settings['imp_channel_ind']]  # available channels
-    if channels_to_plot is None:
-        channels_to_plot = all_calculated_channel_names
+    if color is None:
+        color = all_calculated_channel_names
 
-    for channel_name in channels_to_plot:
+    for channel_name in color:
         if channel_name not in all_calculated_channel_names:
             raise Exception("Channel with name %s not found in \n%s"%(channel_name, all_calculated_channel_names))
 
-    channels_to_plot_ind = [all_channel_names.index(name) for name in channels_to_plot] #all_calculated_channel_names]
+    channels_to_plot_ind = [all_channel_names.index(name) for name in color] #all_calculated_channel_names]
 
 
     # if self.scales[scale_num].X_hsne is None:
@@ -27,7 +27,7 @@ def hsne(adata, channels_to_plot=None, scale_num=-1, subplot_grid_dim=(1, 1)):
     #     self.scales[scale_num].calc_embedding()
 
     # dynamically add rows and cols for subplots
-    num_channels = len(channels_to_plot)
+    num_channels = len(color)
     r, c = subplot_grid_dim
     if r == -1 or c == -1 or r * c < num_channels:
         while r * c < num_channels:
@@ -36,7 +36,8 @@ def hsne(adata, channels_to_plot=None, scale_num=-1, subplot_grid_dim=(1, 1)):
             else:
                 r += 1
     # create subplots
-    for channel in enumerate(channels_to_plot):
+    plt.figure()
+    for channel in enumerate(color):
         plt.subplot(r, c, channel[0] + 1)
         plt.scatter(scales[scale_num].X_hsne[:, 0], scales[scale_num].X_hsne[:, 1],
                     c=scales[scale_num].X[:, channel[0]],
