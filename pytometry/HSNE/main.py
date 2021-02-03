d1995de05bde368cb408e424303ce0304176ef82
Felix-Hmpl <60603136+Felix-Hmpl@users.noreply.github.com>
2021-02-03 15:17:43 +0100

    link to dataset corrected

--- pytometry/HSNE/main.py
+++ pytometry/HSNE/main.py
@@ -7,14 +7,14 @@ import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    filelocation = r"/home/foelix/Öffentlich/VBh_converted.h5ad"
+    filelocation = r"/home/felix/Public/datasets/VBh_converted.h5ad"
     adata = anndata.read_h5ad(filelocation)
 
     imp_channels = [1, 3, 5, 7, 9, 13]
     imp_channels_names = list(adata.var_names.values[imp_channels])
 
     sc.pp.subsample(adata, 0.05)
-    #adata.X = np.arcsinh(adata.X / 10)
+    adata.X = np.arcsinh(adata.X / 10)
     sc.pp.neighbors(adata, n_neighbors=20)
 
 
