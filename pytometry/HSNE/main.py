1ed6347adf3243f047183d76f66d0cd3486d80ef
Felix-Hmpl <60603136+Felix-Hmpl@users.noreply.github.com>
2021-03-09 11:52:58 +0100

    Update main.py

--- pytometry/HSNE/main.py
+++ pytometry/HSNE/main.py
@@ -13,11 +13,12 @@ if __name__ == '__main__':
     imp_channels = [1, 3, 5, 7, 9, 13]
     imp_channels_names = list(adata.var_names.values[imp_channels])
 
-    sc.pp.subsample(adata, 0.05)
+    sc.pp.subsample(adata, 0.04)
+    adata = adata[:, imp_channels]
     adata.X = np.arcsinh(adata.X / 10)
     sc.pp.neighbors(adata, n_neighbors=20)
 
 
-    tl.hsne(adata, imp_channel_ind=imp_channels, num_scales=2)
+    tl.hsne(adata, num_scales=2)
     pl.hsne(adata, channels_to_plot=['FSC-A'], scale_num=0)
 
