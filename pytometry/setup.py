1633c100a4fa0c9283f980a869c574d18f60cdb9
mbuttner <maren.buettner@helmholtz-muenchen.de>
2021-11-11 12:55:51 +0100

    Updated setup.py

--- pytometry/setup.py
+++ pytometry/setup.py
@@ -1,19 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="pytometry", # Replace with your own username
     version="0.0.1",
-    author="Felix Hempel, Thomas Ryborz",
-    author_email="",
+    author="Felix Hempel, Thomas Ryborz, Maren Büttner",
+    author_email="maren.buettner@helmholtz-muenchen.de",
     description="Tools for Flow Cytometry Analysis using the Anndata-dataformat",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+#    long_description=long_description,
+#    long_description_content_type="text/markdown",
     url="https://github.com/pypa/anndata_tools",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
