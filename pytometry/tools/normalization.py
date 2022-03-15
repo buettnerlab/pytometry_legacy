f925f18ee975d4c7c681cba80e8846259de0a431
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-03-15 17:07:40 +0100

    fix typo

--- pytometry/tools/normalization.py
+++ pytometry/tools/normalization.py
@@ -318,7 +318,7 @@ def normalize_biExp(adata,
         adata.X = lut_func(adata.X)
         
     elif len_param == adata.n_vars:
-        for idx, marker in enumerate(adata_tmp.var_names):
+        for idx, marker in enumerate(adata.var_names):
             #get correct row
             row_idx = negative.index == marker
             
